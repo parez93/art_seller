349462afc3df6a4e78513f4ff86eca26c29ea15d
Federico Parezzan <parez93@hotmail.it>
2024-03-14 13:52:33 +0100

    usePathUrlStrategy
    usePathUrlStrategy

--- art_seller_demo/lib/main.dart
+++ art_seller_demo/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
+  usePathUrlStrategy();
   runApp(MyApp());
 }
